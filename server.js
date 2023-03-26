e551076757adb6e260ed2185109fcc96e6a0e230
Detsibli <detsibli@gmail.com>
2023-03-26 10:43:13 -0500

    Stuff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mime = require('mime');
 const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -144,6 +144,4 @@ app.get('/search', (req, res) => {
 
 // Start the server
 app.set('view engine', 'ejs');
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
